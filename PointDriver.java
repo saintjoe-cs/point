2845fd92309404987996346eda55cc9afe2b1240
Brian Capouch <brianc@saintjoe.edu>
2015-01-28 16:03:05 -0500

    Save my students some typing

---
+++ PointDriver.java
@@ -0,0 +1,8 @@
+public class Point {
+	public double x,y;
+
+	public void printPoint() {
+	  System.out.println("X coordinate: " +  this.x);
+	  System.out.println("Y coordinate: " +  this.y);
+	}
+}
