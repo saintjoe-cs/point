d43ba8912b3b7071417895feaebd1bee9a9962a3
Brian Capouch <brianc@saintjoe.edu>
2015-01-30 15:14:26 -0500

    Re-do broken code

--- PointDriver.java
+++ PointDriver.java
@@ -1,8 +1,19 @@
-public class Point {
-	public double x,y;
+public class PointDriver {
 
-	public void printPoint() {
-	  System.out.println("X coordinate: " +  this.x);
-	  System.out.println("Y coordinate: " +  this.y);
+	public static void main(String[] args ) {
+	Point lowerLeft = new Point();
+	Point upperRight = new Point();
+	Point middlePoint = new Point();
+
+	lowerLeft.x = 0.0;
+	lowerLeft.y = 0.0;
+
+	upperRight.x = 1280.0;
+	upperRight.y = 1024.0;
+
+	middlePoint.x = 640.0;
+	middlePoint.y = 512.0;
+
+	lowerLeft.printPoint();
 	}
 }
