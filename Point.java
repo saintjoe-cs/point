47cbee69549e1cc0dcde8fb564acd4b1c9c06d3e
Brian Capouch <brianc@saintjoe.edu>
2015-02-04 15:24:48 -0500

    Bring this up to date with Capouch repo

--- Point.java
+++ Point.java
@@ -1,8 +1,27 @@
+/* Point class for CMP112 
+	Brian Capouch
+	30 January 2015 
+*/
+
 public class Point {
 	public double x,y;
 
+	// Print out the coordinates for a single point
 	public void printPoint() {
 	  System.out.println("X coordinate: " +  this.x);
 	  System.out.println("Y coordinate: " +  this.y);
+	}
+	
+	// Clear x and y values (i.e. reset point to 0)
+	public void clear() {
+	  x = 0.0;
+	  y = 0.0;
+	}
+	
+	// Calculate distance between two points
+	public double distance(Point that) {
+	  double xdiff = x - that.x;
+	  double ydiff = y - that.y;
+	  return Math.sqrt(xdiff * xdiff + ydiff * ydiff);
 	}
 }
